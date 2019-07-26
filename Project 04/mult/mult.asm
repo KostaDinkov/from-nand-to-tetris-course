0ae100baa20e4a7e269ada4e4bc404a4fa211e4e
Kosta <k.v.dinkov@gmail.com>
2019-07-26 14:46:09 +0300

    add: unit4 mult.asm solution

--- Project 04/mult/mult.asm
+++ Project 04/mult/mult.asm
@@ -7,3 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  @0
+  D=M
+  @number
+  M=D
+
+  @1
+  D=M
+  @n
+  M=D
+
+  @i
+  M=0
+
+  @2
+  M=0
+
+  (LOOP)
+  @n
+  D=M
+  
+  @i
+  D=D-M
+  
+  @END
+  D;JEQ
+
+  @number
+  D=M
+  @2
+  M=D+M
+
+  @i
+  D=M
+  M=D+1
+  @LOOP
+  0;JMP
+
+  (END)
+  @END
+  0;JMP
